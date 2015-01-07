fb59dd987297a492f06f36eaafc484890a3d66c4
yzeal <yzeal@live.de>
2015-01-07 21:21:41 +0100

    Ich lad das jetzt nochmal hoch

---
+++ Assets/Scripts/PickupItem.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupItem : MonoBehaviour {
+
+//	private GameObject player;
+	private GameObject hand;
+
+	private bool pickedUp;
+
+	// Use this for initialization
+	void Start () {
+//		player = GameObject.FindWithTag("Player");
+		hand = GameObject.FindWithTag("Hand");
+	}
+	
+	// TODO Drop() sollte vom Player aufgerufen werden.
+	void Update () {
+		if(pickedUp && Input.GetKeyDown("h")){
+			Drop();
+		}
+	}
+
+	public void Drop(){
+		SphereCollider[] sc = GetComponents<SphereCollider>();
+		foreach(SphereCollider sphereCol in sc){
+			sphereCol.enabled = true;
+		}
+		transform.parent = null;
+		rigidbody.isKinematic = false;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(!pickedUp && other.CompareTag("Player")){
+			pickedUp = true;
+			transform.position = hand.transform.position;
+			transform.parent = hand.transform;
+			rigidbody.isKinematic = true;
+			SphereCollider[] sc = GetComponents<SphereCollider>();
+			foreach(SphereCollider sphereCol in sc){
+				sphereCol.enabled = false;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.CompareTag("Player")){
+			pickedUp = false;
+		}
+	}
+}
