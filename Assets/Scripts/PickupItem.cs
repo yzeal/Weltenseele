c7628889a03a553191741117745cfbb0ff12462b
yzeal <yzeal@live.de>
2015-01-30 10:05:54 +0100

    Yeva hält Sachen (relativ) richtig in der Hand

--- Assets/Scripts/PickupItem.cs
+++ Assets/Scripts/PickupItem.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class PickupItem : MonoBehaviour {
 
+	public float angle = 0f;
+	public float offset = 0.1f;
+
+	private Quaternion newRotation = Quaternion.identity;
+
 //	private GameObject player;
 	private GameObject hand;
 
@@ -35,13 +40,21 @@ public class PickupItem : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		if(!pickedUp && other.CompareTag("Player")){
 			pickedUp = true;
-			transform.position = hand.transform.position;
+			transform.position = new Vector3(hand.transform.position.x, hand.transform.position.y + offset, hand.transform.position.z);
 			transform.parent = hand.transform;
 			rigidbody.isKinematic = true;
 			MeshCollider[] sc = GetComponents<MeshCollider>();
 			foreach(MeshCollider sphereCol in sc){
 				sphereCol.enabled = false;
 			}
+		}
+	}
+
+	void OnTriggerStay(Collider other){
+		if(pickedUp && other.CompareTag("Player")){
+			newRotation.eulerAngles = new Vector3(hand.transform.rotation.x + angle, hand.transform.rotation.y, hand.transform.rotation.z);
+			transform.rotation = hand.transform.rotation;
+			transform.localRotation = newRotation;
 		}
 	}
 
